fixed sign of tensor derivatives versus gradients. moved some input complexity into the roa module. tested/added benchmark of old HOOH data.

cfour.py
@@ -1,21 +1,56 @@
 import subprocess
 import numpy as np
 import re
+from qcelemental import molutil
 
 class CFOUR(object):
 
   def format_basis(orig):
       return orig.upper().replace('D-AUG','DA').replace('CC-','')
 
-  def __init__(self, xyz, symbols, keywords, title, executable=None):
+  def __init__(self, xyz, symbols, wfn, keywords, title="title", executable=None):
     self.xyz = xyz
     self.symbols = symbols
-    self.keywords = keywords.copy()
+    self.keywords = {}
+    for k,v in keywords.items():
+        self.keywords[k.upper()] = v.upper() if isinstance(v,str) else v
 
-    if self.keywords['BASIS'] == 'SPECIAL':
-        if 'SPECIAL_BASIS' not in self.keywords:
-            raise Exception('Must provide SPECIAL_BASIS')
-        self.keywords['SPECIAL_BASIS'] = CFOUR.format_basis(self.keywords['SPECIAL_BASIS'])
+    keys = self.keywords.keys()
+    if 'CALCLEVEL' not in keys:
+        self.keywords['CALCLEVEL']=wfn.upper()
+    if 'MEMORY_SIZE' not in keys:
+        self.keywords['MEMORY_SIZE']=10
+    if 'MEM_UNIT' not in keys:
+        self.keywords['MEM_UNIT']='GB'
+    if 'COORDINATES' not in keys: 
+        self.keywords['COORD']='CARTESIAN'
+    if 'SCF_CONV' not in keys: 
+        self.keywords['SCF_CONV']=11
+    if 'SCF_DAMP' not in keys: 
+        self.keywords['SCF_DAMP']=100
+    if 'SCF_EXPSTART' not in keys: 
+        self.keywords['SCF_EXPSTART']=100
+    if 'SCF_MAXCYC' not in keys: 
+        self.keywords['SCF_MAXCYC']=300
+    if 'UNITS' not in keys:  # changing CFOUR default!
+        self.keywords['UNITS']='BOHR'
+
+    # The following basis sets are non-native to CFOUR.
+    bs_special_to_C4 = [
+    '6-31+G*', '6-31++G*', 'asp-cc-pvdz', 'd-aug-cc-pvdz', 'd-aug-cc-pvtz',
+    'orp', 'sadlej-lpol-ds', 'sadlej-lpol-dl', 'sadlej-lpol-fs', 'sadlej-lpol-fl',
+    '6-311++G(3df,3pd)' ]
+
+    # weird names with special formatting simplified in custom GENBAS
+    bs_c4_weird = { '6-311++G(3df,3pd)' : 'POPLE-BIG' }
+
+    if self.keywords['BASIS'] in bs_special_to_C4:
+        BS = self.keywords['BASIS']
+        if BS in bs_c4_weird:
+            self.keywords['SPECIAL_BASIS'] = bs_c4_weird[BS]
+        else:
+            self.keywords['SPECIAL_BASIS'] = CFOUR.format_basis(BS)
+        self.keywords['BASIS'] = 'SPECIAL'
     else:
         self.keywords['BASIS'] = CFOUR.format_basis(self.keywords['BASIS'])
         if 'SPECIAL_BASIS' in self.keywords:
@@ -24,11 +59,23 @@ class CFOUR(object):
     
     self.keywords['COORD'] = 'CARTESIAN'
     self.title = title
-    self.executable = 'cfour' if executable is None else executable
+    if executable == None:
+        self.executable = '/Users/rking/bin/run-cfour'
+    else:
+        self.executable = 'cfour'
 
-  def run(self, scratchName=None):
+
+  def run(self, calctype=None, scratchName=None, read_E=False, read_Gx=False):
     if scratchName is None:
         scratchName = self.title.replace(' ','')
+    if calctype.upper() == 'GRADIENT':
+        if 'DERIV_LEVEL' not in self.keywords:
+            self.keywords['DERIV_LEVEL'] = 1
+    elif calctype.upper() == 'HESSIAN':
+        if 'DERIV_LEVEL' not in self.keywords:
+            self.keywords['DERIV_LEVEL'] = 2
+        if 'VIB' not in self.keywords:
+            self.keywords['VIB'] = 'EXACT'
     self.makeZMAT()
     # print("Launching CFOUR executable...")
     # returns CompletedProcess()
@@ -37,6 +84,50 @@ class CFOUR(object):
         print(rc.stdout)
         print(rc.stderr)
         raise Exception('Bad return code from CFOUR')
+    if calctype.upper() == 'ENERGY':
+        return self.parseFinalEnergyFromOutput()
+    elif calctype.upper() == 'GRADIENT':
+        E = self.parseFinalEnergyFromOutput()
+        (Zs, coord, grad) = self.parseGRD()
+        rmsd, mill = molutil.B787(self.xyz, coord, None, None,
+          mols_align=1e-12, atoms_map=True, verbose=False)
+        RotMat = mill.rotation
+        grad2 = np.dot(mill.rotation, grad.T).T
+        diff = np.max(np.abs(grad - grad2))
+        grad[:] = grad2
+        #print(f'Max change from alignment of gradient {diff:10.5e}')
+        #x = np.dot(mill.rotation, coord.T).T # reproduces self.xyz
+        return E, grad
+    elif calctype.upper() == 'HESSIAN':
+        c4h = self.read_hessian()  # read the hessian from FCMFINAL
+        #print('original H')
+        #print(c4h)
+        coord = self.parseGeometryFromOutput()
+        rmsd, mill = molutil.B787(coord, self.xyz, None, None,
+          mols_align=1e-12, atoms_map=True, verbose=False)
+        c4h2 = mill.align_hessian(c4h)
+        diff = np.max(np.abs(c4h - c4h2))
+        c4h[:] = c4h2
+        #print(f'Max change from alignment of Hessian {diff:10.5e}')
+
+        if read_E:
+            E = self.parseFinalEnergyFromOutput()
+        if read_Gx:
+            (Zs, coord, grad) = self.parseGRD()
+            rmsd, mill = molutil.B787(self.xyz, coord, None, None,
+              mols_align=1e-12, atoms_map=True, verbose=False)
+            RotMat = mill.rotation
+            grad[:] = np.dot(mill.rotation, grad.T).T
+        if read_E and read_Gx:
+            return (E, grad, c4h)
+        elif read_E:
+            return (E, c4h)
+        elif read_Gx:
+            return (grad, c4h)
+        else:
+            return c4h
+    return rc
+
 
   def makeZMAT(self, fp=None):
     s = self.title + '\n'
@@ -75,10 +166,10 @@ class CFOUR(object):
     f.close()
     return
 
-  def parseHessian(self):
+  def read_hessian(self, infile='FCMFINAL.out'):
       Natom = len(self.symbols)
       H = np.zeros( (3*Natom*Natom,3) )
-      with open('FCMFINAL.out','r') as f:
+      with open(infile,'r') as f:
           f.readline()
           for row, line in enumerate(f):
             v = line.split()
@@ -114,7 +205,7 @@ class CFOUR(object):
             mu_z[at][2] = v[3]
       return (mu_x, mu_y, mu_z)
 
-  def parseGeometry(self):
+  def parseGeometryFromOutput(self):
       with open('output.dat','r') as f:
           outtext = f.read()
           mobj = re.search(
@@ -132,20 +223,20 @@ class CFOUR(object):
                   xyz[row][2] = lline[-1]
               return xyz
 
-  def writeFile15(self, H):
+  def writeFile15(self, H, outfile='file15.dat'):
       Natom = len(self.symbols)
       H2 = H.reshape((3*Natom*Natom,3))
       s = ''
-      with open('file15.dat','w') as f15:
+      with open(outfile,'w') as f15:
           for row in range(len(H2)):
               s += "%20.10f%20.10f%20.10f\n" % (H2[row][0],H2[row][1],H2[row][2])
           f15.write(s)
       return
 
-  def writeFile17(self, mu_x, mu_y, mu_z):
+  def writeFile17(self, mu_x, mu_y, mu_z, outfile='file17.dat'):
       Natom = len(self.symbols)
       s = ''
-      with open('file17.dat','w') as f17:
+      with open(outfile, 'w') as f17:
           for row in range(len(mu_x)):
               s += "%20.10f%20.10f%20.10f\n" % (mu_x[row][0],mu_x[row][1],mu_x[row][2])
           for row in range(len(mu_y)):
@@ -155,4 +246,34 @@ class CFOUR(object):
           f17.write(s)
       return
 
+  def parseGRD(self):
+      Natom = len(self.symbols)
+      geom = np.zeros( (Natom,3) )
+      grad = np.zeros( (Natom,3) )
+      Zs   = np.zeros( (Natom) )
+
+      with open('GRD','r') as f: 
+          v = f.readline() 
+          for atom in range(Natom):
+            v = f.readline().split()
+            Zs[atom]      = float(v[0])
+            geom[atom][0] = float(v[1])
+            geom[atom][1] = float(v[2])
+            geom[atom][2] = float(v[3])
+          for atom in range(Natom):
+            v = f.readline().split()
+            grad[atom][0] = float(v[1])
+            grad[atom][1] = float(v[2])
+            grad[atom][2] = float(v[3])
+      return (Zs, geom, grad)
+  
+  def parseFinalEnergyFromOutput(self, fname='output.dat'):
+      rval = 0.0
+      with open(fname,'r') as f:
+          for line in f:
+              v = line.split()
+              if len(v) > 4:
+                  if v[0:5] == ['The','final','electronic','energy','is']:
+                      rval=float(v[5])
+      return rval
 

mode_scatter.py
@@ -334,12 +334,16 @@ def modeScatter(
         pr("%15.10f%15.10f%15.10f\n" % (geom[a,0],geom[a,1],geom[a,2]))
 
     # COMPUTE TENSOR DERIVATIVES
+    # August 2023: Adding negative sign to finite-differences so these
+    # are technically derivatives, not gradients.  This has no effect on
+    # the products of derivatives that appear in the VROA parameters.
+    #
     # A_grad = Array of length # of Cartesian coordinates with respect
     # to which derivative is taken.  Each member is a 3x3 ndarray for the
     #  various electric-dipole/electric-dipole polarizability components.
     A_grad = []
     for i in range(0, len(A_fd), 2):
-        grad_mat = np.subtract( A_fd[i], A_fd[i+1] )
+        grad_mat = -np.subtract( A_fd[i], A_fd[i+1] )
         grad_mat[:] /=  (2.0 * step)
         A_grad.append(grad_mat)
 
@@ -348,7 +352,7 @@ def modeScatter(
     #  various electric-dipole/magnetic-dipole polarizability components.
     G_grad = []
     for i in range(0, len(G_fd), 2):
-        grad_mat = np.subtract( G_fd[i], G_fd[i+1] )
+        grad_mat = -np.subtract( G_fd[i], G_fd[i+1] )
         grad_mat[:] /=  (2.0 * step)
         G_grad.append(grad_mat)
 
@@ -357,7 +361,7 @@ def modeScatter(
     #  various electric quadropole/electric-dipole polarizability components.
     Q_grad = []
     for i in range(0, len(G_fd), 2):
-        grad_mat = np.subtract( Q_fd[i], Q_fd[i+1] )
+        grad_mat = -np.subtract( Q_fd[i], Q_fd[i+1] )
         grad_mat[:] /=  (2.0 * step)
         Q_grad.append(grad_mat)
 

psi4_read.py
@@ -14,9 +14,9 @@
 import numpy as np
 import os
 
-def psi4_read_hessian(Natom):
+def psi4_read_hessian(Natom, infile15='file15.dat'):
     H = np.zeros( (3*Natom*Natom, 3) )
-    with open("file15.dat", "r") as fHessian:
+    with open(infile15, "r") as fHessian:
         for i in range(3*Natom*Natom):
             s = fHessian.readline().split()
             if i==0 and len(s)==2:
@@ -32,9 +32,9 @@ def check_symmetric(a, rtol=1e-05, atol=1e-08):
     return np.allclose(a, a.T, rtol=rtol, atol=atol)
 
 # Read in the Dipole-Moment Derivatives from file 17.
-def psi4_read_dipole_derivatives(Natom):
+def psi4_read_dipole_derivatives(Natom, fname='file17.dat'):
     dipder = np.zeros( (3, 3*Natom) )
-    with open("file17.dat", "r") as fMu:
+    with open(fname, "r") as fMu:
         for i in range(3):  # d(mu_x), d(mu_y), d(mu_z)
             for j in range(Natom):
                 s = fMu.readline().split()